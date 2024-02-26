7507a6dd90f2c94b39f1e8da24d185d518949709
Guillermo Cardoze <vital_tmv@yahoo.com>
2024-02-26 14:08:15 -0600

    complete

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">Abou
+  <h2>About Me</h2>
+  <p>Hello I am Memo.</p>
+  <img src= {image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
